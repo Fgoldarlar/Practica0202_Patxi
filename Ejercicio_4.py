2eaa96bb50133594b5312fb87607de4c3d3bfe3d
Fgoldarlar <fgoldarlar@educacion.navarra.es>
2023-10-27 11:23:39 +0200

    Create Ejercicio_4.py

---
+++ Ejercicio_4.py
@@ -0,0 +1,7 @@
+Edad = int(input("Cuantos años tienes?"))
+Ingresos = float(input("Cuanto ganas al mes?"))
+if  Edad > 16 and Ingresos > 1000:
+    print("A cotizar chaval")
+else: 
+    print("No tienes que cotizar")
+                 
