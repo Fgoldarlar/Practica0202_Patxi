6bd498622a4fcb23c4cbd99ab9bdb4903b037686
Fgoldarlar <fgoldarlar@educacion.navarra.es>
2023-10-27 10:26:21 +0200

    Create Ejercicio_1.py

---
+++ Ejercicio_1.py
@@ -0,0 +1,6 @@
+Osasuna = input("Buenas, pon su contraseña para continuar: ")
+Contraseña = "manolo"
+if Contraseña == Osasuna.lower():
+     print("Acceso permitido")
+else: 
+     print ("Acceso denegado")
