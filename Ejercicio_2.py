e8406749891174b5d761eba60ab19d059cdeb77f
Fgoldarlar <fgoldarlar@educacion.navarra.es>
2023-10-27 10:40:15 +0200

    Create Ejercicio_2.py

---
+++ Ejercicio_2.py
@@ -0,0 +1,7 @@
+A = float(input("Digame un número: "))
+B = float(input("Digame su divisor: "))
+Resultado = (input(A/B))
+if B == 0: 
+    print("El resultado no es válido ya que el divisor es 0")
+else: 
+    print(Resultado)
